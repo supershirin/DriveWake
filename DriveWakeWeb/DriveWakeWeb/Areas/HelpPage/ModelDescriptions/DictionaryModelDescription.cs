a7201d25ff0011333ee96ed0bdd1e6a6474f4499
lezhanger <lisa4474water@yahoo.ca>
2015-11-14 16:18:35 -0800

    added web

---
+++ DriveWakeWeb/DriveWakeWeb/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DriveWakeWeb.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ DriveWakeWeb/DriveWakeWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DriveWakeWeb.WebApiApplication" Language="C#" %>
