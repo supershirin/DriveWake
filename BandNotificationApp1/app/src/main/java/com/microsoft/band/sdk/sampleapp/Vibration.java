78566521e589f67278df410f75b8c78885067714
Katherine Huang <kjhuang523@gmail.com>
2015-11-15 09:23:52 -0500

    got the right code

---
+++ BandNotificationApp1/app/src/main/java/com/microsoft/band/sdk/sampleapp/Vibration.java
@@ -0,0 +1,12 @@
+package com.microsoft.band.sdk.sampleapp;
+
+/**
+ * Created by catherinehuang on 2015-11-14.
+
+public class Vibration {
+    private int intensity;
+     public void setIntensity(int intensity1){
+        intensity
+    }
+}
+ */
